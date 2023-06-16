e677e1c40e10f77cca6384b88a108cf22d975a97
hatsumi3 <al.usamiku39@gmail.com>
2023-06-16 21:23:53 +0900

    feat: call weather api.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
-import './App.css';
+import { useState } from "react";
+
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Result from "./components/Result";
+import "./App.css";
+
+type ResultStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+};
 
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
+
+  const getWeather = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        })
+      );
+  };
+
   return (
-    <div className="test">
-      <h1>こんにちは</h1>
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Result results={results} />
+      </div>
     </div>
   );
 }
